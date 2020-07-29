e0e2166e4fd343913bd51ec17d02a58367aa6af9
Subrat Singhal <subrat.singhal0130@gmail.com>
2020-07-29 20:34:19 +0530

    Updated Clima App

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -28,6 +30,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02#');
     if (response.statusCode == 200) {
       String data = response.body;
+      var decodedData = jsonDecode(data);
+      int id = decodedData['weather'][0]['id'];
+      double temperature = decodedData['main']['temp'];
+      String cityName = decodedData['name'];
+      print(id.toString() + ' ' + temperature.toString() + ' ' + cityName);
     } else {
       print(response.statusCode);
     }
